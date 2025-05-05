5c641ff3ca4f6b1e57c0db661ac8ba1aaceefc3c
B1bb3 <138508000+B1bb3@users.noreply.github.com>
2025-05-05 13:13:21 +0200

    Update DictionaryWithAI.py

--- DictionaryWithAI.py
+++ DictionaryWithAI.py
@@ -1,11 +1,3 @@
-""" import tesseract
-import pyautogui
-from pynput.mouse import Listener
-from PIL import Image
-import time 
-import pyperclip 
-from pynput import keyboard  """
-# husk at lave limit så de ikke kan sende 1 mil tokens på en gang.
 from tkinter import *
 import threading
 import pyperclip
@@ -21,10 +13,10 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 
 previous_copied_word = pyperclip.paste()
 copied_word = None
-button_pressed = True
-bg_color = "#2C2C2C"
 
 client = OpenAI(api_key='')
+
+# Used to decide where to spawn the window and take the screenshot
 
 start_x = None
 start_y = None
@@ -39,34 +31,34 @@ last_time_clicked = None
 gui_spawn_x = 100
 gui_spawn_y = 100
 
+# Styling
+bg_color = "#2C2C2C"
 
 def CheckClipboardContinuosly():
-    global previous_copied_word, copied_word, button_pressed
+    global previous_copied_word, copied_word
     while True:
-        time.sleep(0.2)
-
         copied_word = pyperclip.paste()
-        if (previous_copied_word != copied_word and button_pressed):
+        if (previous_copied_word != copied_word):
 
             # Take screenshot of screen
-            screenshot = pyautogui.screenshot(region=(start_x, start_y, 1720, 800))
+            screenshot = pyautogui.screenshot(region=(0, start_y-100, 1920, 150))
             screenshot.save("screenshot.png")
 
             text = pytesseract.image_to_string(screenshot)
-
-
-            print(text)
+            print("Converted screenshot to string")
             
             # Send request to OpenAI's API
             response = client.responses.create(
                 model="gpt-4.1-nano-2025-04-14",
-                input=copied_word,
+                input=f"The word: {copied_word}. Context: {text}",
                 instructions="Explain the word accurately, briefly, and concisely."
             ) 
 
+            # Show word on the screen
             word_meaning = response.output[0].content[0].text
-            UpdateGUI(word_meaning)
+            root.after(1, UpdateGUI(word_meaning))
             
+            # Update previous word copied
             previous_copied_word = copied_word
             
 def UpdateGUI(word_meaning):
@@ -75,21 +67,23 @@ def UpdateGUI(word_meaning):
         widget.destroy()
     label = Label(frame, text=word_meaning, bg=bg_color, fg="white")
     label.grid(column=0, row=0, sticky="nsew")
-    update_window_position()
+    root.after(1, update_window_position())
 
 def update_window_position():
     global gui_spawn_x, gui_spawn_y
 
-    # Update window position here (you can adjust this to your logic)
+    # Update window position here
     root.geometry(f"+{gui_spawn_x}+{gui_spawn_y}")
+    root.deiconify() # because of quirks in how overrideredirect, geometry, and visibility interact, it forces the window manager to reprocess positioning and appearance.
     root.attributes("-topmost", True)
 
-
-
+def change_topmost(bool):
+    root.attributes("-topmost", bool)
 
 def OnClick(mouse_x, mouse_y, mouse_button, pressed):
     global start_x, start_y, width, height, last_time_clicked, gui_spawn_x, gui_spawn_y, double_click_detected, marked_word_detected
     if pressed:
+        change_topmost(False)
         now = time.time()
         # Is it a double click?
         if last_time_clicked is not None:
@@ -106,30 +100,30 @@ def OnClick(mouse_x, mouse_y, mouse_button, pressed):
         start_y = mouse_y
         
     else:
-        width = abs(mouse_x-start_x)
-        height = abs(mouse_y-start_y)
+        if start_x is None or start_y is None:
+            return
+        width = abs(mouse_x - start_x)
+        height = abs(mouse_y - start_y)
 
         if (width > 0 or height > 0):
-            gui_spawn_x = start_x
-            gui_spawn_y = start_y
             print("User has marked word")
             marked_word_detected = True
         else:
             print("User has not marked word")
             marked_word_detected = False
 
-    if (marked_word_detected):
-        min_x = min(start_x, mouse_x)
-        min_y = min(start_y, mouse_y)
-        app_width = root.winfo_width()
-        app_height = root.winfo_height()
+        if (marked_word_detected):
 
-        gui_spawn_y = abs(min_y-app_height) - 50
-        gui_spawn_x = min_x
+            min_x = min(start_x, mouse_x)
+            min_y = min(start_y, mouse_y)
+            app_height = root.winfo_height()
+            gui_spawn_y = abs(min_y-app_height) - 20
+            gui_spawn_x = abs(min_x - 50) 
 
 
 # === Main Frontend ===
 root = Tk()
+root.withdraw()
 # root.geometry(f"500x70+{gui_spawn_x}+{gui_spawn_y}")
 root.overrideredirect(True)
 root.configure(bg=bg_color)
@@ -149,6 +143,7 @@ close_button = Button(title_bar, text="✕", bg="#1F1F1F", fg="white", font=("Ar
                       bd=0, highlightthickness=0, command=root.destroy, activebackground="#3A3A3A")
 close_button.pack(side=RIGHT, padx=5)
 
+# Possible to drag application 
 def start_move(event):
     root.x = event.x
     root.y = event.y
@@ -185,54 +180,13 @@ mouse_listener.start()
 root.mainloop()
 
 
+# === Things that can be improved or are missing ===
 
-"""def OnPress(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
-
-def OnRelease(key):
-    print(f'Key released: {key}')
-    if key == keyboard.Key.esc:
-        # Stop listener
-       return False
-"""
-
-"""
-start_x = None
-start_y = None
-screenshot = None
-
-def on_click(x, y, button, pressed):
-    global start_x, start_y, screenshot
-
-    if (pressed):
-        start_x = x
-        start_y = y
-        print(f"Button pressed at: {start_x}, {start_y}")
-
-    else: 
-        end_x = x
-        end_y = y
-
-        left = min(start_x, end_x)
-        top = min(start_y, end_y)
-        width = abs(end_x - start_x)
-        height = abs(end_y - start_y)
-        if width > 0 and height > 0:
-            screenshot = pyautogui.screenshot(region=(left, top, width, height))
-            print("Screenshot taken")    
-            screenshot.save("SelectedArea.png")
-            print("Screenshot saved")
-
-
-listener = Listener(on_click=on_click)
-listener.start()
-listener.join()
-"""
-
-"""listener = keyboard.Listener(on_press=OnPress, on_release=OnRelease)
-listener.start() """
+# Limit, so users can't send too many tokens at once - right now they could potentially send 1 million tokens at once or more
+# If the explanation is x long make it span one more line
+# Choose between output languages
+# Choose between short and long answers
+# Make it possible for users to write their own instructions
+# Possible to write with the bot
+# Choose another hotkey than CTRL + C
+# Update the position of where the window should spawn if the click was a double click
